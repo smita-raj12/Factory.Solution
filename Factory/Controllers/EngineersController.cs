f28c255175c3bc1ad26d14a0f961a694169ccf86
smita Raj <smita.raj12@gmail.com>
2021-10-21 16:46:24 -0700

    add edit functionality on engineer controller and view file

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -45,5 +45,24 @@ namespace Factory.Controllers
           .FirstOrDefault(Engineer => Engineer.EngineerId == id);
       return View(thisEngineer);
     }
+     public ActionResult Edit(int id)
+    {
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineType");
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer, int MachineId)
+    {
+      if (MachineId != 0)
+      {
+        _db.MachineEngineer.Add(new MachineEngineer() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+      }
+      _db.Entry(engineer).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }    
