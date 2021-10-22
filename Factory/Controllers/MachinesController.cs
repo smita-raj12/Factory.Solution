5ceb8f2b404b29b31d9987bd96766e168c7195f9
smita Raj <smita.raj12@gmail.com>
2021-10-21 19:07:25 -0700

    add funcyionality to avoid duplicate value

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -31,12 +31,19 @@ namespace Factory.Controllers
     [HttpPost]
     public ActionResult Create(Machine machine, int EngineerId)
     {
-      _db.Machines.Add(machine);
-       _db.SaveChanges();
+      List<Machine> MachinesList = _db.Machines.ToList();
+      foreach(Machine m in MachinesList)
+      {
+        if(machine.MachineType != m.MachineType)
+        {
+          _db.Machines.Add(machine);
+          _db.SaveChanges();
           if (EngineerId != 0)
           {
             _db.MachineEngineer.Add(new MachineEngineer() { EngineerId = EngineerId, MachineId = machine.MachineId });
-          }  
+          } 
+        }  
+      }     
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
