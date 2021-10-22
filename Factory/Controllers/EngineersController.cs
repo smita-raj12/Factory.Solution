8ebf2988ac8476889b2825d3d2c21c7dcfbb0a21
smita Raj <smita.raj12@gmail.com>
2021-10-21 17:08:37 -0700

    fix the bug

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -95,6 +95,13 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
+    [HttpPost]
+    public ActionResult DeleteMachine(int joinId)
+    {
+      var joinEntry = _db.MachineEngineer.FirstOrDefault(entry => entry.MachineEngineerId == joinId);
+      _db.MachineEngineer.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }    
